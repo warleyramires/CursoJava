86a7966eaddfe98b9e9fe31ff848ca2694b90079
Warley Ramires <warleyramiresgoncalves@gmail.com>
2023-10-20 11:45:52 -0300

    Bad solution Exemplo004

--- s15_tratamento_de_excecoes/Exemplo_004/src/Main.java
+++ s15_tratamento_de_excecoes/Exemplo_004/src/Main.java
@@ -31,15 +31,15 @@ public class Main {
         System.out.print("Check-out date (dd/MM/yyyy): ");
         checkOut = sdf.parse(sc.next());
 
-        Date now = new Date();
-        if(checkIn.before(now) || checkOut.before(now)){
-            System.out.println("Error in reservation: Reservation dates for update must be future");
-        }else if(!checkOut.after(checkIn)){
-            System.out.println("Error in reservation: Check-out date must be after check-in date");
-        }
 
-        reservation.upDateDates(checkIn, checkOut);
-        System.out.println("Reservation" + reservation);
+
+        String error = reservation.upDateDates(checkIn, checkOut);
+        if(error != null){
+            System.out.println("Error in reservation" + error);
+        }else{
+            System.out.println("Reservation" + reservation);
+
+        }
 
     }
 
