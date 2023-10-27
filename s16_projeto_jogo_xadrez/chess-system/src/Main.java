3aac8c6a0dbc319ad56407938b91ece452a81b1d
Warley Ramires <warleyramiresgoncalves@gmail.com>
2023-10-27 12:47:09 -0300

    First class: Position

--- s16_projeto_jogo_xadrez/chess-system/src/Main.java
+++ s16_projeto_jogo_xadrez/chess-system/src/Main.java
@@ -1,8 +1,12 @@
+import boardgame.Position;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        
+        Position pos = new Position(3, 5);
+
+        System.out.println(pos);
+
     }
 }
